89f46263eb97afb945f7e28a172a3dd641fa9217
kiwidaikon <niratamagomoti@yahoo.co.jp>
2025-01-13 19:48:12 +0900

    掃除をしてresltだけ修正する状態

--- app/Http/Controllers/CalcController.php
+++ app/Http/Controllers/CalcController.php
@@ -6,20 +6,8 @@ use Illuminate\Http\Request;
 
 class CalcController extends Controller
 {
-    public function hello()
-    {
-        return view('calcs.hello');
-    }
-    public function var()
-    {
-        return view('calcs.var', ['variable' => 'Hello from CalcController']);
-    }
-    public function word($number_1, $score, $number_2)
-    {
-        return view('calcs.word2', ['number_1' => $number_1, 'score' => $score, 'number_2' => $number_2]);
-    }
     public function result($number_1, $score, $number_2)
     {
-        return view('calcs.resurt', ['number_1' => $number_1, 'score' => $score, 'number_2' => $number_2]);
+        return view('calcs.result', ['number_1' => $number_1, 'score' => $score, 'number_2' => $number_2]);
     }
 }
--- resources/views/calcs/result.blade.php
+++ resources/views/calcs/result.blade.php
@@ -5,26 +5,16 @@
      <meta charset="UTF-8">
      <title>計算アプリ</title>
  </head>
-
- //<?php
- //echo '$number_1, $score, $number_2';
- //if ({$score}= "addition") {$keisan={ $number_1 }+{ $number_2 }}
- //<p><?php echo $kasan;
- //
- ?></p>
-
- // } elseif ({$score}= "subtraction") {$keisan={ $number_1 }-{ $number_2 }}
- // <p><?php echo $kasan; ?></p>
- //
- //} elseif ({$score}= "multiplication") {$keisan={ $number_1 }*{ $number_2 }}
- // 処理3
- //<p><?php echo $kasan; ?></p>
- //} else {
- //$keisan={ $number_1 }/{ $number_2
+ //<? php
+ //if ({$score}= "addition") {$keisan={ $number_1 }-{ $number_2 }
+ //<p><?php echo '$keisan'; ?></P>
  //}
- //<p><?php echo $kasan; ?></p>
-
- <body>
-     <h1>{{ $number_1 }}、{{ $score }}、{{ $number_2 }}入力</h1>
- </body>
- </body>
+ //elseif ({$score}= "subtraction") {$keisan={ $number_1 }-{ $number_2 }
+ //<p><?php echo '$keisan'; ?></p>
+ //}
+ elseif ({$score}= "multiplication") {$keisan={ $number_1 }*{ $number_2 }
+ //<?php echo '$keisan'; ?></p>
+ //}
+ //else { $keisan={ $number_1 }/{ $number_2 }
+ //<p><?php echo '$keisan'; ?></p>
+ //}
