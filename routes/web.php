f4d15ec0d83df4a163060457ad182aa45e64dde7
kiwidaikon <niratamagomoti@yahoo.co.jp>
2025-01-13 12:21:05 +0900

    ビューの追加まで

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::<HTTPメソッド>('<アドレス>', クロージャ);
+Route::get('/route/hello', function () {
+    return '<h1>Hello from Route!</h1>';
+});
+
+Route::get('/view/hello', function () {
+    return view('message.hello');
+});
