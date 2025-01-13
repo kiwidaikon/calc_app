7fb9e34e30bc8f18e491ea066fad4b8d0509754b
kiwidaikon <niratamagomoti@yahoo.co.jp>
2025-01-13 13:02:04 +0900

    ビューのみ２要素表示まで

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Route::<HTTPメソッド>('<アドレス>', クロージャ);
-Route::get('/route/hello', function () {
-    return '<h1>Hello from Route!</h1>';
-});
-
 Route::get('/view/hello', function () {
     return view('message.hello');
+});
+
+Route::get('/view/var', function () {
+    return view('message.var', ['variable' => 'Hello from web.php']);
+});
+// 下記の方法でも可
+// Route::get('/view/var', function () {
+//     $data = ['variable' => 'Hello from web.php'];
+//     return view('message.var', $data);
+// });
+
+Route::get('/view/word/{msg}', function ($msg) {
+    return view('message.word', ['msg' => $msg]);
+});
+
+
+Route::get('/view/word/{name}/{msg}', function ($name, $msg) {
+    return view('message.word2', [
+        'name' => $name,
+        'msg' => $msg
+    ]);
 });
