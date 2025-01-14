18f5285f03b4bcc7ede83c66dfe264192fcdc07a
kiwidaikon <niratamagomoti@yahoo.co.jp>
2025-01-14 20:23:17 +0900

    →

--- app/Http/Controllers/CalcController.php
+++ app/Http/Controllers/CalcController.php
@@ -6,20 +6,20 @@ use Illuminate\Http\Request;
 
 class CalcController extends Controller
 {
-    public function result($number_1, $score, $number_2)
+    public function result($number_1, $operator, $number_2)
     {
         
     
-         if ($score == 'addition') {
+         if ($operator == 'addition') {
          $ans = $number_1 + $number_2 ;
-     } elseif ($score == 'subtraction') {
+     } elseif ($operator == 'subtraction') {
          $ans = $number_1 - $number_2 ;
-     } elseif ($score == 'multiplication') {
+     } elseif ($operator == 'multiplication') {
          $ans = $number_1 * $number_2 ;
-     } elseif ($score = 'division') {
+     } elseif ($operator = 'division') {
          $ans = $number_1 / $number_2 ;
 } 
-return view('calcs.result', ['number_1' => $number_1, 'score' => $score, 'number_2' => $number_2,'ans' => $ans]);
+return view('calcs.result', ['number_1' => $number_1, 'operator' => $operator, 'number_2' => $number_2,'ans' => $ans]);
     }
 }
 
